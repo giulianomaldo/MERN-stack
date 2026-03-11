82b6aad466345f67f38e7e16aac5d72ec3494c05
Giuliano <giulianomaldonadorampulla@gmail.com>
2026-03-11 16:46:26 -0300

    Add tailwind and daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+
 
 const App = () => {
-  return <div>
-
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </div>
-  
+  return (
+    // Agregamos className="min-h-screen" justo al lado de tu data-theme
+    <div data-theme="coffee" className="min-h-screen">
+      <button className="btn btn-primary">click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
