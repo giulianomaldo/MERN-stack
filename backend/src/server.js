fee50ed2d49aab79052297e64db63faf2b5eb3af
Giuliano <giulianomaldonadorampulla@gmail.com>
2026-03-13 13:28:49 -0300

    Add loading and limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express"
-import notesRoutes from "./routes/notesRoutes.js"
-import { connectDB } from "./config/db.js";
+import cors from "cors";
 import dotenv from "dotenv";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
@@ -11,8 +14,13 @@ const PORT = process.env.PORT || 5001
 
 
 // middleware
+app.use(
+    cors({
+    origin:"http://localhost:5173",
+}));
 app.use(express.json());
 app.use(rateLimiter);
+
 //app.use((req, res, next) => {
     //console.log(`Req method is ${req.method} & the url is ${req.url}.`);
     //next();
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import Navbar from "../components/Navbar";
+import RateLimitedUI from '../components/RateLimitedUI';
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  const fetchNotes = async () => {
+    try {
+      const res = await axios.get("http://localhost:5001/api/notes");
+
+      console.log(res.data);
+      setNotes(res.data);
+      setIsRateLimited(false);
+    } catch (error) {
+      console.log("Error fetching notes.");
+      console.log(error);
+
+      if (error.response.status === 429) {
+        setIsRateLimited(true);
+      } else {
+        toast.error("Failed to load notes.");
+      }
+
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchNotes();
+}, []);
   return <div className='min-h-screen'>
     <Navbar />
+
+    {isRateLimited && <RateLimitedUI/>}
+
+    <div className='max-w-7xl mx-auto p-4 mt-6'>
+      {loading && <div className='text-center text-primary py-10'>Loading notes...</div> }
+    </div>
   </div>;
 }
 
