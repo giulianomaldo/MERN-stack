531788a437e96c8926f1ed23c0b40573695edccf
Giuliano <giulianomaldonadorampulla@gmail.com>
2026-03-16 16:38:41 -0300

    Finish with the Home Page and staring with the CreatePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 const App = () => {
   return (
     // Agregamos className="min-h-screen" justo al lado de tu data-theme
-    <div data-theme="coffee" className="min-h-screen">
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
